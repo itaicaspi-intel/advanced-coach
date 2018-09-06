538d7d49eec39214756ab99da7d4abcf98d78cf5
itaicaspi-intel <itai.caspi@intel.com>
2018-09-06 14:18:29 +0300

    carla dataset to replay buffer script

---
+++ cil/dataset_to_replay_buffer.py
@@ -0,0 +1,41 @@
+import h5py
+import os
+import sys
+
+from rl_coach.core_types import Transition
+from rl_coach.memories.memory import MemoryGranularity
+from rl_coach.memories.non_episodic.experience_replay import ExperienceReplay
+
+dataset_root = '/home/cvds_lab/Downloads/AgentHuman/'
+
+train_set_root = os.path.join(dataset_root, 'SeqTrain')
+validation_set_root = os.path.join(dataset_root, 'SeqVal')
+
+# training set extraction
+memory = ExperienceReplay(max_size=(MemoryGranularity.Transitions, sys.maxsize))
+train_set_files = sorted(os.listdir(train_set_root))
+print("found {} files".format(len(train_set_files)))
+for file_idx, file in enumerate(train_set_files[:500]):
+    print("extracting file {}: {}".format(file_idx, file))
+    train_set = h5py.File(os.path.join(train_set_root, file), 'r')
+    observations = train_set['rgb'][:]                # forward camera
+    measurements = train_set['targets'][:, 10]        # forward speed
+    actions = train_set['targets'][:, :3]             # steer, gas, break
+    high_level_commands = train_set['targets'][:, 24]  # follow lane, left, right, straight
+
+    file_length = train_set['rgb'].len()
+    assert train_set['rgb'].len() == train_set['targets'].len()
+
+    for transition_idx in range(file_length):
+        transition = Transition(
+            state={
+                'observation': observations[transition_idx],
+                'measurements': measurements[transition_idx],
+                'high_level_command': high_level_commands[transition_idx]
+            },
+            action=actions[transition_idx],
+            reward=0
+        )
+        memory.store(transition)
+
+memory.save('carla_train_set_replay_buffer.p')
